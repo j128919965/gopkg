18f73a4916fe06ad85a6c4dc70a96a375c6392d7
圆鹊 <853681401@qq.com>
2022-03-02 15:21:07 +0800

    auth

--- security/errors.go
+++ security/errors.go
@@ -4,24 +4,24 @@ import "github.com/j128919965/gopkg/errors"
 
 var ErrDidNotLogin = &errors.BizError{
 	Code:  1,
-	Msg:   "没有检测到登录信息",
+	Msg:   "auth:没有检测到登录信息",
 	Cause: nil,
 }
 
 var ErrWrongToken = &errors.BizError{
 	Code:  2,
-	Msg:   "登录信息无效",
+	Msg:   "auth:登录信息无效",
 	Cause: nil,
 }
 
 var ErrNeedFresh = &errors.BizError{
 	Code:  3,
-	Msg:   "令牌需要刷新",
+	Msg:   "auth:令牌需要刷新",
 	Cause: nil,
 }
 
 var ErrNeedLogin = &errors.BizError{
 	Code:  4,
-	Msg:   "需要重新登录",
+	Msg:   "auth:需要重新登录",
 	Cause: nil,
 }
